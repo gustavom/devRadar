7cd3cba54a722a51c8aa49cf854aee4811e4d086
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2020-01-16 02:29:50 -0300

    add crud routes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,13 @@
 const { Router } = require('express');
-
+const DevController = require('./controllers/DevController');
+const SearchController = require('./controllers/SearchController');
 const routes = Router();
 
-routes.post('/users', (req, res) => {
-  res.send({ message: 'hello' });
-});
+routes.get('/devs', DevController.index);
+routes.get('/devs/:github_username', DevController.show);
+routes.delete('/devs/:id', DevController.delete);
+routes.post('/devs', DevController.store);
+
+routes.get('/search', SearchController.index);
 
 module.exports = routes;
